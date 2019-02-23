4fb0543f547a69d337b57b00a37cd27ad7ba9d6d
Maria Wieckowska <mkw98@gazeta.pl>
2019-02-23 20:51:20 +0100

    edit server.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'google_client_id': '327150527966-gf1m78iau89j4mj7nj92k3ijf5r8t32i.apps.googleusercontent.com',
-	'google_client_secret': 'oJtoEhVjN58XfbCWHX6uOXZa',
-	'callback_url': 'http://localhost:3000/auth/google/callback'
+	'GOOGLE_CLIENT_ID': '327150527966-gf1m78iau89j4mj7nj92k3ijf5r8t32i.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'oJtoEhVjN58XfbCWHX6uOXZa',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
