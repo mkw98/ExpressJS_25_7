b34646b896cc15b5b36bc820f48a2c58a2da55fa
Maria Wieckowska <mkw98@gazeta.pl>
2019-02-23 20:18:49 +0100

    init project

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'google_client_id': '327150527966-gf1m78iau89j4mj7nj92k3ijf5r8t32i.apps.googleusercontent.com',
+	'google_client_secret': 'oJtoEhVjN58XfbCWHX6uOXZa',
+	'callback_url': 'http://localhost:3000/auth/google/callback'
+};
